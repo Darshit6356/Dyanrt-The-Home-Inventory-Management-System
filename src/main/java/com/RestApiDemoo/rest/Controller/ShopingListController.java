af79c5d4ae96228e75e114ef4bc03c7e67694b54
Darshit6356 <22ceuog035@ddu.ac.in>
2024-03-26 18:25:16 +0700

    Shoping list added

---
+++ src/main/java/com/RestApiDemoo/rest/Controller/ShopingListController.java
@@ -0,0 +1,46 @@
+package com.RestApiDemoo.rest.Controller;
+
+import com.RestApiDemoo.rest.Model.Item;
+import com.RestApiDemoo.rest.Model.NewItem;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/shopinglist")
+public class ShopingListController {
+
+    @Autowired
+    private ItemController itemsController;
+
+    @Autowired
+    private NewItemController newItemController;
+
+    @GetMapping
+    public List<Item> generateShoppingList() {
+        List<Item> items = itemsController.getItems();
+        List<NewItem> newItems = newItemController.getNewitem();
+
+        List<Item> shoppingList = new ArrayList<>();
+
+        for (NewItem newItem : newItems) {
+            for (Item item : items) {
+                if (newItem.getNewitem_name().equals(item.getName())) {
+                    long neededQuantity = Math.max(0, item.getMin_Q() - newItem.getNewitem_qty());
+                    if (neededQuantity > 0) {
+                        Item shoppingListItem = new Item();
+                        shoppingListItem.setName(item.getName());
+                        shoppingListItem.setMin_Q(neededQuantity);
+                        shoppingList.add(shoppingListItem);
+                    }
+                }
+            }
+        }
+
+        return shoppingList;
+    }
+}
